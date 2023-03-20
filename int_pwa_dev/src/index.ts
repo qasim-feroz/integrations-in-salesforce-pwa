6843b439dac280e194bf7876626272d273dbae89
Qasim Feroz <qasim.feroz@onestepretail.com>
2023-03-20 17:16:56 +0500

    Klaviyo integration update

--- int_pwa_dev/src/index.ts
+++ int_pwa_dev/src/index.ts
@@ -4,6 +4,7 @@ import Access from './components/authorizePayment/token'
 import AuthorizePayment from './components/authorizePayment/authorize'
 import updateAdyenOrderInfo from './components/authorizePayment/updateAdyenOrder'
 import KlaviyoOrderConfirmationMetric from './klaviyo/KlaviyoOrderConfirmationMetric'
+import KlaviyoPasswordResetMetric from './klaviyo/KlaviyoPasswordResetMetric'
 import { OrderResult } from './interface/OrderResult'
 
-export { AdyenCCFields, Access, AuthorizePayment, updateAdyenOrderInfo, KlaviyoOrderConfirmationMetric, OrderResult }
+export { AdyenCCFields, Access, AuthorizePayment, updateAdyenOrderInfo, KlaviyoOrderConfirmationMetric, KlaviyoPasswordResetMetric, OrderResult }
