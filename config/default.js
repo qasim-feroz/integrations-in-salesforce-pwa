3fd74ab931e37ed051f2011a03b1381a01962e6e
m-ahmedbashir <ahmed.bashir@nestosh.com>
2023-05-24 14:40:02 +0500

    Added pwa-core with recaptcha integrations and added googleRecaptcha cred. into default.js

--- config/default.js
+++ config/default.js
@@ -6,7 +6,7 @@ module.exports = {
             // Determine where the siteRef is located. Valid values include 'path|query_param|none'. Defaults to: 'none'
             // site: 'none',
             // Determine where the localeRef is located. Valid values include 'path|query_param|none'. Defaults to: 'none'
-            locale: 'none',
+            locale: 'none'
             // This boolean value dictates whether or not default site or locale values are shown in the url. Defaults to: false
             // showDefaults: true
         },
@@ -33,6 +33,11 @@ module.exports = {
             proxyPath: `/mobify/proxy/einstein`,
             einsteinId: 'undefined',
             siteId: 'RefArch'
+        },
+        googleRecaptcha: {
+            siteKey: '6LeVQxglAAAAALm5-jbgPw5OCn-ws7UKytkV8fAn',
+            secretKey: '6LeVQxglAAAAAMwpUAhoJar-g_TTOxEwjYeNoLnF',
+            verifySiteUrl: 'https://www.google.com/recaptcha/api/siteverify'
         }
     },
     // This list contains server-side only libraries that you don't want to be compiled by webpack
@@ -41,10 +46,10 @@ module.exports = {
     pageNotFoundURL: '/page-not-found',
     // Enables or disables building the files necessary for server-side rendering.
     ssrEnabled: true,
-    // This list determines which files are available exclusively to the server-side rendering system 
+    // This list determines which files are available exclusively to the server-side rendering system
     // and are not available through the /mobify/bundle/ path.
     ssrOnly: ['ssr.js', 'ssr.js.map', 'node_modules/**/*.*'],
-    // This list determines which files are available to the server-side rendering system 
+    // This list determines which files are available to the server-side rendering system
     // and available through the /mobify/bundle/ path.
     ssrShared: [
         'static/ico/favicon.ico',
