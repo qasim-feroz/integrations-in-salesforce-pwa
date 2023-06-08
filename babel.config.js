d8873a6f208058d768f47007de6ce372de0a1fae
Qasim Feroz <qasim_feroz@bitbucket.org>
2023-06-08 17:00:55 +0500

    Adding module resolver for absolute paths

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,22 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-module.exports = require('pwa-kit-dev/configs/babel/babel-config')
+const config = require('pwa-kit-dev/configs/babel/babel-config')
+
+if (config && config.default && config.default.plugins) {
+    config.default.plugins.push(
+        [
+            require.resolve('babel-plugin-module-resolver'),
+            {
+                root: ['./app'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.svg', '.png'],
+                alias: {
+                    App: './app',
+                    hooks: './app/hooks',
+                    components: './app/components',
+                    itemvariant: './app/components/item-variant',
+                }
+            }])
+}
+
+module.exports = config
