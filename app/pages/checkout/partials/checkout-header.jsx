8a50968777eb9400f588a12dfc1753130b83e7f9
Rashid Shahzad Ali <rashid.shahzad@nestosh.com>
2023-07-26 16:25:43 +0500

    cleaned unsed code and updated color schemes

--- app/pages/checkout/partials/checkout-header.jsx
+++ app/pages/checkout/partials/checkout-header.jsx
@@ -5,24 +5,29 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import React from 'react'
-import {FormattedMessage} from 'react-intl'
-import {Badge, Box, Button, Flex, Center} from '@chakra-ui/react'
+import { FormattedMessage } from 'react-intl'
+import { Badge, Box, Button, Flex, Center, Image } from '@chakra-ui/react'
 import useBasket from '../../../commerce-api/hooks/useBasket'
 import Link from '../../../components/link'
-import {BasketIcon, BrandLogo} from '../../../components/icons'
-import {HOME_HREF} from '../../../constants'
+import { BasketIcon, BrandLogo } from '../../../components/icons'
+import { HOME_HREF } from '../../../constants'
+import {getAssetUrl} from 'pwa-kit-react-sdk/ssr/universal/utils'
 
 const CheckoutHeader = () => {
     const basket = useBasket()
     return (
-        <Box px={[4, 4, 8]} bg="white" borderBottom="1px" borderColor="gray.100">
+        <Box px={[4, 4, 8]} bg="#8c3c6c" borderBottom="1px" borderColor="gray.100">
             <Box maxWidth="container.xxxl" marginLeft="auto" marginRight="auto">
-                <Flex h={{base: '52px', md: '80px'}} align="center" justify="space-between">
+                <Flex h={{ base: '52px', md: '80px' }} align="center" justify="space-between">
                     <Link href={HOME_HREF} title="Back to homepage">
-                        <BrandLogo
-                            width={{base: '35px', md: '45px'}}
-                            height={{base: '24px', md: '32px'}}
-                        />
+                        <Box>
+                            <Image
+                                src={getAssetUrl('static/img/nestosh-logo.png')}
+                                alt="brand-logo"
+                                width={'150px'}
+                                cursor={'pointer'}
+                            />
+                        </Box>
                     </Link>
 
                     <Button
@@ -30,7 +35,7 @@ const CheckoutHeader = () => {
                         href="/cart"
                         display="inline-flex"
                         variant="unstyled"
-                        color="gray.900"
+                        color="pinkTant.50"
                         rightIcon={
                             <Center position="relative" width={11} height={11}>
                                 <BasketIcon position="absolute" left="0px" />
